f4c9956331d2c2580bc4f19592dd2e656f6f0c88
Jue-Viole-Grace <fancy199800@gmail.com>
2020-06-30 15:22:54 +0800

    完善入口

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 //1.导入模块
 const express = require('express');
+const cors = require('cors');
 //2.创建服务器
 const app = express();
+
+//post请求参数处理
+// for parsing application/json
+app.use(express.json());
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//解决跨域
+app.use(cors());
+
 //3.开启服务器
 app.listen(8888,() => {
     console.log('running');
