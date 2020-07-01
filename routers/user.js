0b3eaf6cf89b54e30855e23c69ee2b2e64802807
Jue-Viole-Grace <fancy199800@gmail.com>
2020-07-01 15:08:31 +0800

    1508

--- routers/user.js
+++ routers/user.js
@@ -5,22 +5,22 @@ const router = express.Router();
 //路由配置
 
 //获取用户信息接口--------------------------------------------------------
-router.get('userinfo', (req,res) => {
+router.get('/userinfo', (req,res) => {
     res.send('userinfo');
 });
 
 //更新用户信息------------------------------------------------------------
-router.post('userinfo', (req,res) => {
+router.post('/userinfo', (req,res) => {
     res.send('update userinfo');
 });
 
 //更新用户密码------------------------------------------------------------
-router.post('updatepwd', (req,res) => {
+router.post('/updatepwd', (req,res) => {
     res.send('pwd');
 });
 
 //更新用户头像------------------------------------------------------------
-router.post('update/avatar', (req,res) => {
+router.post('/update/avatar', (req,res) => {
     res.send('pic');
 });
 
