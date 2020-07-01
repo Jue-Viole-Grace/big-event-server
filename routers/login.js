e31ab050a33dbffb39c4299cfc238e7b52cdbb4f
Jue-Viole-Grace <fancy199800@gmail.com>
2020-07-01 09:36:06 +0800

    注册接口完成

--- routers/login.js
+++ routers/login.js
@@ -8,8 +8,24 @@ router.post('/login',(req,res) => {
     res.send('login');
 });
 
-router.post('/register',(req,res) => {
-    res.send('register');
+//用户注册接口
+router.post('/register',async (req,res) => {
+    //获取前端传递过来的参数
+    let param = req.body;
+    //调用数据库相关的sql语句
+    let sql = 'insert into user set ?';
+    let ret = await db.operateData(sql,param);
+    if(ret && ret.affectedRows > 0){
+        res.json({
+            status: 0,
+            message: '注册成功'
+        });
+    }else{
+        res.json({
+            status: 1,
+            message: '注册失败'
+        });
+    };
 });
 
 router.get('/test', async (req,res) => {
