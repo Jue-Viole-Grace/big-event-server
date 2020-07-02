bf55cc9b2a2cd87046e45a1f89979777d112facd
Jue-Viole-Grace <fancy199800@gmail.com>
2020-07-02 09:55:29 +0800

    用户相关接口完成

--- routers/user.js
+++ routers/user.js
@@ -1,6 +1,7 @@
 //统一管理路由信息
 const express = require('express');
 const path = require('path');
+const utils = require('utility');
 const db = require(path.join(__dirname,'../common/self-operateData-Promise'));
 const router = express.Router();
 
@@ -26,19 +27,67 @@ router.get('/userinfo', async (req,res) => {
     };
 });
 
-//更新用户信息------------------------------------------------------------
-router.post('/userinfo', (req,res) => {
-    res.send('update userinfo');
+//更改用户信息接口------------------------------------------------------------
+router.post('/userinfo', async (req,res) => {
+    //获取请求参数
+    let param = req.body;
+    let sql = 'update user set ? where id = ?';
+    let ret = await db.operateData(sql,[{nickname: param.nickname,email: param.email},param.id]);
+    if(ret && ret.affectedRows > 0){
+        res.json({
+            status: 0,
+            message: '修改用户信息成功'
+        });
+    }else{
+        res.json({
+            status: 1,
+            message: '修改用户信息失败'
+        });
+    };
 });
 
-//更新用户密码------------------------------------------------------------
-router.post('/updatepwd', (req,res) => {
-    res.send('pwd');
+//更改用户密码接口------------------------------------------------------------
+router.post('/updatepwd', async (req,res) => {
+    //获取请求参数
+    let param = req.body;
+    //新旧密码加密
+    param.oldPwd = utils.md5(param.oldPwd);
+    param.newPwd = utils.md5(param.newPwd);
+    //获取用户id
+    let id = req.user.id;
+    let sql = 'update user set password = ? where id = ? and password = ?';
+    let ret = await db.operateData(sql,[param.newPwd,id,param.oldPwd]);
+    if(ret && ret.affectedRows > 0){
+        res.json({
+            status: 0,
+            message: '修改密码成功'
+        });
+    }else{
+        res.json({
+            status: 1,
+            message: '修改密码失败'
+        });
+    };
 });
 
-//更新用户头像------------------------------------------------------------
-router.post('/update/avatar', (req,res) => {
-    res.send('pic');
+//更新用户头像接口------------------------------------------------------------
+router.post('/update/avatar', async (req,res) => {
+    //获取请求参数
+    let param = req.body;
+    let id = req.user.id;
+    let sql = 'update user set user_pic = ? where id = ?';
+    let ret = await db.operateData(sql,[param.avatar,id]);
+    if(ret && ret.affectedRows > 0){
+        res.json({
+            status: 0,
+            message: '更新头像成功'
+        });
+    }else{
+        res.json({
+            status: 1,
+            message: '更新头像失败'
+        });
+    };
 });
 
 
