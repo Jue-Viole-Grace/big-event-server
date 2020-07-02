7ce123ea573477b0e94e86638daca949846a1540
Jue-Viole-Grace <fancy199800@gmail.com>
2020-07-02 14:57:48 +0800

    查询列表

--- routers/article.js
+++ routers/article.js
@@ -5,9 +5,12 @@ const db = require(path.join(__dirname,'../common/self-operateData-Promise'));
 const router = express.Router();
 
 //获取文章列表接口-----------------------------------------------------------
-router.get('/cates', async (req,res) => {
-    let sql = 'select * from article';
-    let ret = await db.operateData(sql,null);
+router.get('/list', async (req,res) => {
+    let param = req.query;
+    param.pagenum = parseInt(param.pagenum);
+    param.pagesize = parseInt(param.pagesize);
+    let sql = 'select * from article limit ?, ?';
+    let ret = await db.operateData(sql,[param.pagesize*(param.pagenum-1),param.pagesize]);
     if(ret && ret.length > 0){
         res.json({
             status: 0,
@@ -20,6 +23,26 @@ router.get('/cates', async (req,res) => {
             message: '获取文章列表失败'
         });
     };
+});
+
+//发表文章------------------------------------------------------------------
+router.post('/add', async (req,res) => {
+    let param = req.body;
+});
+
+//删除文章-----------------------------------------------------------------
+router.get('/delete/:id', async (req,res) => {
+    let id = req.params.id;
+});
+
+//根据id查询文章-----------------------------------------------------------
+router.get('/:id', async (req,res) => {
+    let id = req.params.id;
+});
+
+//更新文章-----------------------------------------------------------------
+router.post('/edit', async (req,res) => {
+    let param = req.body;
 });
 
 
