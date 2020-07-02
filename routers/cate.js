12f4d01c696c8f331b546b122393eceb98a73e16
Jue-Viole-Grace <fancy199800@gmail.com>
2020-07-02 10:58:45 +0800

    文章分类结构

--- routers/cate.js
+++ routers/cate.js
@@ -4,24 +4,29 @@ const path = require('path');
 const db = require(path.join(__dirname,'../common/self-operateData-Promise'));
 const router = express.Router();
 
+//获取文章分类
 router.get('/cates', async (req,res) => {
     res.send('cates');
 });
 
+//添加文章分类
 router.get('/addcates', async (req,res) => {
     res.send('addcates');
 });
 
+//删除文章分类
 router.get('/deletecate/:id', async (req,res) => {
     res.send('delete');
 });
 
+//
 router.get('/cates/:id', async (req,res) => {
     res.send('delete');
 });
 
+//更新文章分类
 router.get('/updatecate', async (req,res) => {
-    res.send('delete');
+    res.send('update');
 });
 
 
