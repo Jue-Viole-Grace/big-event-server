e51202407b28c4c665878df5fea4c71ef2e5d04d
Jue-Viole-Grace <fancy199800@gmail.com>
2020-07-04 10:56:23 +0800

    发表文章完成

--- routers/article.js
+++ routers/article.js
@@ -20,11 +20,12 @@ router.get('/list', async (req,res) => {
         };
     };
     //去掉最后一个and
-    condition = condition.substring(0,condition.lastIndexOf('and'));1
+    //condition = condition.substring(0,condition.lastIndexOf('and'));1
 
-    let sql = 'select * from article limit ?, ?';
-    let totalSql = 'select count(*) as total from article';
+    let sql = 'select * from article where is_delete = 0 limit ?, ?';
+    let totalSql = 'select count(*) as total from article where is_delete = 0';
     if(condition){
+        condition += ' is_delete = 0 ';
         sql = 'select * from article where '+ condition +' limit ?, ?';
         totalSql = 'select count(*) as total from article where ' + condition;
     };
@@ -45,19 +46,46 @@ router.get('/list', async (req,res) => {
     };
 });
 
+//删除文章-----------------------------------------------------------------
+router.get('/delete/:id', async (req,res) => {
+    let id = req.params.id;
+    let sql = 'update article set is_delete = 1 where id = ?';
+    let ret = await db.operateData(sql,id);
+    if(ret && ret.affectedRows > 0){
+        res.json({
+            status: 0,
+            message: '删除成功'
+        });
+    }else{
+        res.json({
+            status: 1,
+            message: '删除失败'
+        });
+    };
+});
+
 //发表文章------------------------------------------------------------------
 router.post('/add', async (req,res) => {
     let param = req.body;
 });
 
-//删除文章-----------------------------------------------------------------
-router.get('/delete/:id', async (req,res) => {
-    let id = req.params.id;
-});
-
 //根据id查询文章-----------------------------------------------------------
 router.get('/:id', async (req,res) => {
     let id = req.params.id;
+    let sql = 'select * from article where is_delete = 0 and id = ?';
+    let ret = await db.operateData(sql,id);
+    if(ret && ret.length > 0){
+        res.json({
+            status: 0,
+            message: '查询成功',
+            data: ret[0]
+        });
+    }else{
+        res.json({
+            status: 1,
+            message: '查询失败'
+        });
+    };
 });
 
 //更新文章-----------------------------------------------------------------
